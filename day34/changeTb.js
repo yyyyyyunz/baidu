13419730cb3ffcd86a76c416ac9ab95b2a515925
yyyyyyunz <36809005+yyyyyyunz@users.noreply.github.com>
2018-06-12 10:23:19 +0800

    Create changeTb.js

---
+++ day34/changeTb.js
@@ -0,0 +1,12 @@
+function changeTb(){
+	let ipts1=regionCheck.querySelectorAll("input[type=checkbox]:checked");
+	let ipts2=productCheck.querySelectorAll("input[type=checkbox]:checked");
+	let tab=document.querySelector("#table");
+	if(ipts1.length===1&&ipts2.length!==1){
+		for(let i=0;i<tab.rows.length;i++){
+			let temp=tab.rows[i].cells[0].innerHTML;
+			tab.rows[i].cells[0].innerHTML=tab.rows[i].cells[1].innerHTML;
+			tab.rows[i].cells[1].innerHTML=temp;
+		}
+	}
+}
